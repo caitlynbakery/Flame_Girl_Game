f64a66d12cb9e201c1a96e0be1be50a4c8dc5a2b
Caitlyn <craigoda+caitlyn@gmail.com>
2021-03-22 07:38:02 -0700

    added background image

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,50 @@ void main() {
 }
 
 class MyApp extends BaseGame with DoubleTapDetector {
-  SpriteComponent girl = SpriteComponent();
+  SpriteComponent boy = SpriteComponent();
   SpriteAnimationComponent girlAnimation = SpriteAnimationComponent();
   String direction = 'right';
   bool running = true;
+  Sprite starSprite;
 
   @override
   Future<void> onLoad() async {
     print('loading assets');
+
+
+    SpriteComponent background = SpriteComponent()
+      ..sprite = await loadSprite('background.png')
+      ..size = size;
+
+    add(background);
     print(size);
 
-    girl
-      ..sprite = await loadSprite('girl.png')
-      ..size = Vector2(153, 160)
-      ..x = 100.0
-      ..y = 60.0;
+    starSprite = await loadSprite('Star.png');
 
-    // add(girl);
+    SpriteComponent star = SpriteComponent()
+      ..sprite = starSprite
+      ..size = Vector2(50, 50)
+      ..x = 200
+      ..y = 250;
+
+    add(star);
+
+    boy
+      ..sprite = await loadSprite('boy.png')
+      ..size = Vector2(150.0, 150.0)
+      ..x = size[0] - 200
+      ..y = 200.0;
+
+    add(boy);
 
     var spriteSheet = await images.load('spritesheet.png');
-    final spriteSize = Vector2(150, 163);
+    final spriteSize = Vector2(150 * 0.7, 163 * 0.7);
     SpriteAnimationData spriteData = 
     SpriteAnimationData.sequenced(amount: 20, stepTime: 0.03, textureSize: Vector2(150, 163));
 
     girlAnimation = SpriteAnimationComponent.fromFrameData(spriteSheet, spriteData)
       ..x = 100
-      ..y = size[1] - 163/2
+      ..y = size[1] - 163
       ..anchor = Anchor.center
       ..size = spriteSize;
 
